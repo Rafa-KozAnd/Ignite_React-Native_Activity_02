521136fe7ae51705400febec16981a32ba5ce4e3
Rafael Kozlowski Andreola <rafandreola@hotmail.com>
2023-03-16 18:13:04 -0300

    Add files via upload

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import { SignIn } from '../screens/SignIn'
+
+const { Navigator, Screen } = createStackNavigator()
+
+export function AuthRoutes() {
+  return(
+    <Navigator>
+      <Screen 
+        name="SignIn"
+        component={SignIn}
+        options={{
+          headerShown: false
+        }}
+      />
+    </Navigator>
+  )
+}
